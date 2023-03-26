008f5c71402b3ee8d69541f9ad4b43fa4c0c500d
Svgta <svgta@live.com>
2023-03-26 20:27:52 +0200

    Delete tests directory

--- tests/createKeys.php
+++
@@ -1,6 +0,0 @@
-<?php
-require dirname(__FILE__, 2) . '/vendor/autoload.php';
-$key = Svgta\OidcUtils::genRsaKey();
-print_r($key); echo PHP_EOL;
-$key = Svgta\OidcUtils::genEcKey();
-print_r($key); echo PHP_EOL;
